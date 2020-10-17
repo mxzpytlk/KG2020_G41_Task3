d90d4aee2a046d32a3f7228f24cfe2d05e6f0e51
Denis Rybas <denis2002.rybas@gmail.com>
2020-10-17 10:06:20 +0300

    lesson task

--- src/rybas/DrawPanel.java
+++ src/rybas/DrawPanel.java
@@ -5,6 +5,7 @@ import rybas.linedrawers.LineDrawer;
 import rybas.models.Line;
 import rybas.pixeldrawers.BufferedImagePixelDrawer;
 import rybas.pixeldrawers.PixelDrawer;
+import rybas.points.ScreenPoint;
 
 import javax.swing.*;
 import java.awt.*;
@@ -37,12 +38,37 @@ public class DrawPanel extends JPanel implements MouseMotionListener, MouseListe
         sc.setScreenW(getWidth());
         bi_g.fillRect(0, 0, getWidth(), getHeight());
         bi_g.setColor(Color.black);
+        drawAxes(ld);
         drawAll(ld);
         bi_g.dispose();
         g.drawImage(bi, 0, 0, null);
     }
 
     private void drawAll(LineDrawer ld) {
+//        int w = this.getWidth()/2;
+//        int h = this.getHeight()/2;
+//
+//        Graphics2D g1 = (Graphics2D) g;
+//        g1.setStroke(new BasicStroke(2));
+//        g1.setColor(Color.black);
+//        g1.drawLine(0,h,w*2,h);
+//        g1.drawLine(w,0,w,h*2);
+//        g1.drawString("0", w - 7, h + 13);
+//
+//        int scale = 10;
+//        for (int x = 0; x <= 4; x++) {
+//            p.addPoint(w+scale*x, h - scale*((x*x*x) + x - 3));
+//        }
+////...lines skipped
+//        Polygon p1 = new Polygon();
+//        for (int x = -10; x <= 10; x++) {
+//            p1.addPoint(w + scale*x, h - scale*((x*x*x)/100) - x + 10);
+//        }
+//
+        drawAxes(ld);
+    }
+
+    private void drawAxes(LineDrawer ld) {
         ld.drawLine(sc.realToScreen(xAxis.getP1()), sc.realToScreen(xAxis.getP2()));
         ld.drawLine(sc.realToScreen(yAxis.getP1()), sc.realToScreen(yAxis.getP2()));
         for (Line l : lines) {
@@ -55,19 +81,18 @@ public class DrawPanel extends JPanel implements MouseMotionListener, MouseListe
     public void mouseMoved(MouseEvent e) {
     }
 
-//    private ScreenPoint prevDrag;
+    private ScreenPoint prevDrag;
 
     @Override
     public void mouseClicked(MouseEvent e) {
-//        prevDrag
+        prevDrag = new ScreenPoint(e.getX(), e.getY());
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
-//
-//        sc.setX();
-//        sc.setY();
-//        repaint();
+        ScreenPoint current = new ScreenPoint(e.getX(), e.getY());
+        ScreenPoint delta = new ScreenPoint(current.getX() - prevDrag.getX(), current.getY() - prevDrag.getY());
+        repaint();
     }
 
     @Override
